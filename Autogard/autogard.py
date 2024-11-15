5e1b059bb24b2fcf48ac8d9d9eba813e8f7e772c
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-11-15 01:52:04 +0100

    created autograd file

---
+++ Autogard/autogard.py
@@ -0,0 +1,30 @@
+import torch
+
+# requires_grad = True -> tracks all operations on the tensor. 
+x = torch.randn(3, requires_grad=True)
+y = x + 2
+
+# y was created as a result of an operation, so it has a grad_fn attribute.
+# grad_fn: references a Function that has created the Tensor
+print(x) # created by the user -> grad_fn is None
+print(y)
+print(y.grad_fn)
+
+# Do more operations on y
+z = y * y * 3
+print(z)
+z = z.mean()
+print(z)
+
+
+# Let's compute the gradients with backpropagation
+# When we finish our computation we can call .backward() and have all the gradients computed automatically.
+# The gradient for this tensor will be accumulated into .grad attribute.
+# It is the partial derivate of the function w.r.t. the tensor
+
+print(x.grad)
+z.backward()
+print(x.grad) # dz/dx
+
+# !!! Careful!!! backward() accumulates the gradient for this tensor into .grad attribute.
+# !!! We need to be careful during optimization !!! optimizer.zero_grad()

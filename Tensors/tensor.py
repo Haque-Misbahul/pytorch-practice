7e055f1f8a5358340d4c04d45626e8b9c2585624
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-11-15 01:22:06 +0100

    Tensor demo practice

--- Tensors/tensor.py
+++ Tensors/tensor.py
@@ -19,3 +19,30 @@ print("rand(5,3):", x)
 # torch.ones(size), fill with 1
 x = torch.zeros(5, 3)
 print("zeros(5,3):", x)
+
+
+# check size
+print("size", x.size())  # x.size(0)
+print("shape", x.shape)  # x.shape[0]
+
+# check data type
+print(x.dtype)
+
+# specify types, float32 default
+x = torch.zeros(5, 3, dtype=torch.float16)
+print(x)
+
+# check type
+print(x.dtype)
+
+# construct from data
+x = torch.tensor([5.5, 3])
+print(x, x.dtype)
+
+
+# requires_grad argument
+# This will tell pytorch that it will need to calculate the gradients for this tensor
+# later in your optimization steps
+# i.e. this is a variable in your model that you want to optimize
+x = torch.tensor([5.5, 3], requires_grad=True)
+print(x)
